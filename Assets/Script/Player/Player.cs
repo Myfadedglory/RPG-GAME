bd90ac4b9ae2ea9299e3bbac1ed68cee5bf02165
Myfadedglory <150012213+Myfadedglory@users.noreply.github.com>
2024-09-18 17:59:47 +0800

    basically move and jump

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -1,18 +1,96 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [Header("Move info")]
+    public float moveSpeed = 3.80f;
+    public float horizonJumpForce = 8f;
+    public float verticalJumpForce = 10f;
+
+    [Header("Collision Check")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private float wallCheckDistance;
+
+    public int facingDir { get; private set; } = 1;
+    public bool facingRight = true;
+
+
+    #region Component
+
+    public Animator anim {get ;private set;}
+    public Rigidbody2D rb { get ; private set;}
+
+    #endregion
+
+    public PlayerStateMachine stateMachine {  get; private set; }
+
+
+    #region State
+
+    public PlayerIdleState idleState { get; private set; }
+    public PlayerMoveState moveState { get; private set; }
+    public PlayerJumpState jumpState { get; private set; }
+    public PlayerAirState airState { get; private set; }
+
+    #endregion
+    private void Awake()
     {
-        
+        stateMachine = new PlayerStateMachine();
+
+        idleState = new PlayerIdleState(this , stateMachine , "Idle");
+        moveState = new PlayerMoveState(this , stateMachine , "Move");
+        jumpState = new PlayerJumpState(this , stateMachine , "Jump");
+        airState = new PlayerAirState(this , stateMachine , "Jump");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        anim = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+
+        stateMachine.Initialize(idleState);
     }
+
+    private void Update()
+    {
+        stateMachine.currentState.Update();
+    }
+
+    public void SetVelocity(float _xVelocity , float _yVelocity)
+    {
+        rb.velocity = new Vector2(_xVelocity, _yVelocity);
+        FlipController(_xVelocity);
+    }
+
+    public bool IsGroundDetected() => Physics2D.Raycast(groundCheck.position,Vector2.down , groundCheckDistance , whatIsGround);
+    public bool IsWallDetected() => Physics2D.Raycast(wallCheck.position, Vector2.right, wallCheckDistance , whatIsGround);
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
+        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));
+    }
+
+    #region Flip
+
+    public void Flip()
+    {
+        facingDir *= -1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    public void FlipController(float _x)
+    {
+        if (rb.velocity.x > 0 && !facingRight)
+            Flip();
+        else if(rb.velocity.x < 0 && facingRight)
+            Flip();
+    }
+
+    #endregion
 }
--- Assets/Script/Player/PlayerState.cs
+++ Assets/Script/Player/PlayerState.cs
@@ -1,18 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerState : MonoBehaviour
+public class PlayerState
 {
-    // Start is called before the first frame update
-    void Start()
+    protected PlayerStateMachine stateMachine;
+    protected Player player;
+
+    protected Rigidbody2D rb;
+
+    protected float xInput;
+    private string animBoolName;
+
+    public PlayerState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName)
     {
-        
+        this.player = _player;
+        this.stateMachine = _stateMachine;
+        this.animBoolName = _animBoolName;
     }
 
-    // Update is called once per frame
-    void Update()
+    public virtual void Enter()
     {
-        
+        player.anim.SetBool(animBoolName, true);
+        rb = player.rb;
+    }
+
+    public virtual void Update()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+
+        player.anim.SetFloat("yVelocity",rb.velocity.y);
+    }
+
+    public virtual void Exit()
+    {
+        player.anim.SetBool(animBoolName, false);
     }
 }
